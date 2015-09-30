5bfe549b61da51307696c3c0dc28e113a0e75da5
alexismaldonado <alexismaldonado112@gmail.com>
2015-09-30 02:55:30 +0000

    Initial commit

--- taxixpress.sql
+++ taxixpress.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 5.5.43, for debian-linux-gnu (x86_64)
 --
--- Host: 0.0.0.0    Database: taxixpressapp_development
+-- Host: localhost    Database: taxixpressapp_development
 -- ------------------------------------------------------
 -- Server version	5.5.43-0ubuntu0.14.04.1
 
@@ -14,6 +14,69 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `autos`
+--
+
+DROP TABLE IF EXISTS `autos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `autos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `Marca` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `Modelo` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `Año` date DEFAULT NULL,
+  `Placa` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `Capacidad` int(11) DEFAULT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `autos`
+--
+
+LOCK TABLES `autos` WRITE;
+/*!40000 ALTER TABLE `autos` DISABLE KEYS */;
+INSERT INTO `autos` VALUES (1,'Kia','Rio','2015-09-23','NG-5210',6,'2015-09-23 14:51:50','2015-09-23 14:51:50');
+/*!40000 ALTER TABLE `autos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `chofers`
+--
+
+DROP TABLE IF EXISTS `chofers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `chofers` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `Nombre` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `Apellido` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `DNI` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `Direccion` text COLLATE utf8_unicode_ci,
+  `Telefono` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `auto_id` int(11) DEFAULT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `index_chofers_on_auto_id` (`auto_id`),
+  CONSTRAINT `fk_rails_3a1aa8e814` FOREIGN KEY (`auto_id`) REFERENCES `autos` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `chofers`
+--
+
+LOCK TABLES `chofers` WRITE;
+/*!40000 ALTER TABLE `chofers` DISABLE KEYS */;
+INSERT INTO `chofers` VALUES (1,'Julian','Martel','44151200','E. Barreda 451 La Victoria','987451200',1,'2015-09-23 18:06:52','2015-09-23 18:06:52');
+/*!40000 ALTER TABLE `chofers` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `clientes`
@@ -35,7 +98,7 @@ CREATE TABLE `clientes` (
   `created_at` datetime NOT NULL,
   `updated_at` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -44,6 +107,7 @@ CREATE TABLE `clientes` (
 
 LOCK TABLES `clientes` WRITE;
 /*!40000 ALTER TABLE `clientes` DISABLE KEYS */;
+INSERT INTO `clientes` VALUES (2,'Alexis','Maldonado','42184511','+51966358324','Teofilo Castillo\r\n664','alexismaldonado112@tqc.com.pe','Sauron','P@ssw0rd','2015-09-23 04:20:22','2015-09-23 04:20:22');
 /*!40000 ALTER TABLE `clientes` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -66,7 +130,7 @@ CREATE TABLE `schema_migrations` (
 
 LOCK TABLES `schema_migrations` WRITE;
 /*!40000 ALTER TABLE `schema_migrations` DISABLE KEYS */;
-INSERT INTO `schema_migrations` VALUES ('20150918215431'),('20150919020525'),('20150919230945');
+INSERT INTO `schema_migrations` VALUES ('20150918215431'),('20150919020525'),('20150919230945'),('20150922152445'),('20150923053348'),('20150923071158'),('20150923135427'),('20150923140521'),('20150923140724');
 /*!40000 ALTER TABLE `schema_migrations` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -79,4 +143,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2015-09-20  0:02:14
+-- Dump completed on 2015-09-30  2:55:13
